54ef320fe2434f93a6be4f2cbeffb32f33b9882d
gabfv <gab.github@gmail.com>
2023-09-27 05:50:13 -0400

    Removed lib that became edge function

--- src/libs/emailDomain.js
+++
@@ -1,9 +0,0 @@
-let ACCEPTED_EMAIL_DOMAINS_CSV = import.meta.env.VITE_ACCEPTED_EMAIL_DOMAINS_CSV;
-
-//split the accepted email domains into an array
-const ACCEPTED_EMAIL_DOMAINS = ACCEPTED_EMAIL_DOMAINS_CSV.split(',');
-
-export default function validateEmailDomain(email) {
-    const emailDomain = email.split('@')[1];
-    return ACCEPTED_EMAIL_DOMAINS.includes(emailDomain);
-}
