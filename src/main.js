a9e7a1f7b29d15b99d7b0322edc7f1f2499d0619
gabfv <gab.github@gmail.com>
2023-09-27 05:34:35 -0400

    Added Vercel analytics

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue';
 import ToastPlugin from 'vue-toast-notification';
 import 'vue-toast-notification/dist/theme-bootstrap.css';
+import { inject } from '@vercel/analytics';
 
 import App from './App.vue';
 import router from './router';
+
+inject();
 
 const app = createApp(App);
 
